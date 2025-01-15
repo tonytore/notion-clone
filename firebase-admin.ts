d421ed51e5bcf101fb88305f281af1a8a6695162
tonytor <teme4842@gmail.com>
2025-01-15 19:47:30 +0300

    edit firebase admin fiile

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,17 +1,19 @@
-import { initializeApp,getApps,getApp,cert } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
-
-
+import { initializeApp, getApps, getApp, cert } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 
 // Your service account key JSON file
-const serviceAccount = require('./service_key.json');
+const serviceAccount = require("./service_key.json");
 
 // Initialize Firebase Admin SDK
-const app = getApps.length === 0 ? initializeApp({
-    credential: cert(serviceAccount),
-    databaseURL: 'https://tonytor-notion-clone-default-rtdb.europe-west1.firebasedatabase.app'
-}):getApp()
+const app =
+  getApps.length === 0
+    ? initializeApp({
+        credential: cert(serviceAccount),
+        databaseURL:
+          "https://tonytor-notion-clone-default-rtdb.europe-west1.firebasedatabase.app",
+      })
+    : getApp();
 
 const admin_db = getFirestore(app);
 
-export  {app as adminApp,admin_db }
+export { app as adminApp, admin_db };
