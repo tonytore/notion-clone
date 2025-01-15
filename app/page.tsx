532417f25181f27c3644d196778c58d2cfd22942
tonytor <teme4842@gmail.com>
2025-01-15 19:59:12 +0300

    updating some bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-   <div className="border  min-h-screen p-3">
-    hello me
-   </div>
-  );
+  return <div className="border  min-h-screen p-3">hello me</div>;
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,33 +1,31 @@
-"use client"
+"use client";
 
 import {
-    ClerkProvider,
-    SignInButton,
-    SignedIn,
-    SignedOut,
-    UserButton,
-    useUser
-  } from '@clerk/nextjs'
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
 
 const Header = () => {
- const user = useUser()
- 
+  const user = useUser();
 
   return (
-  
-    <section className='flex items-center justify-between p-5 border '>
-      <h1>{user.isSignedIn ? `${user.user?.firstName}'s Space` : 'Notion Clone'} </h1>
+    <section className="flex items-center justify-between p-5 border ">
+      <h1>
+        {user.isSignedIn ? `${user.user?.firstName}'s Space` : "Notion Clone"}{" "}
+      </h1>
       <div>
-      <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </section>
+  );
+};
 
-  )
-}
-
-export default Header
+export default Header;
